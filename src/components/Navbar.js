d4ca92ae42bec177f76e0469abcff2347ac64d56
Lepha <lepharamchiary@gmail.com>
2024-04-04 19:47:41 +0530

    add one more class

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,7 +16,7 @@ class Navbar extends Component {
                 <div className="menu-icon" onClick={this.handleClick}>
                     <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
                 </div>
-                <ul className="nav-menu">
+                <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
                     {MenuItem.map((item, index) => {
                         return (
                             <li>
