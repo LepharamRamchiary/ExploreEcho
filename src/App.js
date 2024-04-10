d0911867c0a5f56b2bdeae737536981642a8328f
Lepha <lepharamchiary@gmail.com>
2024-04-10 20:08:07 +0530

    add route and routes for switing the page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import "./index.css"
-import Navbar from "./components/Navbar"
+// import Navbar from "./components/Navbar"
+import Home from "./routes/Home";
+import { Route, Routes } from "react-router-dom";
+import About from "./routes/About";
+import Service from "./routes/Service";
+import Contact from "./routes/Contact";
 function App() {
   return (
-    <div className="App">
-      <Navbar />
+    <div className="app">
+      {/* <Navbar /> */}
+      <Routes>
+        <Route path="/" element = {<Home />} />
+        <Route path="/about" element= {<About />} />
+        <Route path="/service" element= {<Service />} />
+        <Route path="/contact" element = {<Contact />} />
+      </Routes>
+      
+      
     </div>
   );
 }
