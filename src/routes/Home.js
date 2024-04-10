85aafc1c6ac20a55959de4af45f70c818c4ef9cb
Lepha <lepharamchiary@gmail.com>
2024-04-10 20:08:53 +0530

    adding a new page

---
+++ src/routes/Home.js
@@ -0,0 +1,11 @@
+import Navbar from "../components/Navbar";
+function Home() {
+    return (
+        <>
+            <Navbar />
+            <h1>This is our Home</h1>
+        </>
+    )
+}
+
+export default Home;
