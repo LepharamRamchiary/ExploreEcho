b6e27a8ed388d600db2be7a130d8e1da2fed4a3c
Lepha <lepharamchiary@gmail.com>
2024-04-23 17:18:38 +0530

    destination

---
+++ src/components/Destination.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Kamakhya from "../assets/KamakhyaTemple.jpg"
+import Kamakhya1 from "../assets/kamakhya.jpg"
+
+const Destination = () => {
+    return (
+        <div className='destination'>
+            <h1>Popular Destinations</h1>
+            <p>Tours give you the opportunity to see a lot, within a time frame.</p>
+            <div className='first-des'>
+                <div className='des-text'>
+                    <h2>Kamakhya Temple,Guwahati</h2>
+                    <p>The Kamakhya Temple at Nilachal hills in Guwahati, Assam is one of the oldest and most revered centres of Tantric practices,[3] dedicated to the goddess Kamakhya. The temple is the center of the Kulachara Tantra Marga and the site of the Ambubachi Mela, an annual festival that celebrates the menstruation of the goddess.</p>
+                </div>
+
+                <div className='image'>
+                    <img src={Kamakhya} alt='image'/>
+                    <img src={Kamakhya1} alt='image'/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Destination
