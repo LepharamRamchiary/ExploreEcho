f184cfc9a636bb12d67ea9ab34982949a28688f0
Lepha <lepharamchiary@gmail.com>
2024-04-24 16:16:48 +0530

    new page

---
+++ src/components/Data.js
@@ -0,0 +1,11 @@
+import { Component } from "react"
+import "./DestinationStyle.css"
+import { render } from "@testing-library/react"
+
+class Data extends Component {
+    render() {
+        return (
+
+        )
+    }
+}
