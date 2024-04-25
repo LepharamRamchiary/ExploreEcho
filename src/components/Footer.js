536b9cdb85ccb802777f652802e80c886b3152ca
Lepha <lepharamchiary@gmail.com>
2024-04-25 13:10:15 +0530

    this page for footer section

---
+++ src/components/Footer.js
@@ -0,0 +1,24 @@
+import "./FooterStyle.css";
+
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="top">
+        <div>
+            <h1>ExploreEcho</h1>
+            <p>Choose your favourite destination</p>
+        </div>
+        <div>
+            
+        </div>
+      </div>
+
+      <div className="bottom"></div>
+    </div>
+  )
+}
+
+export default Footer
+
