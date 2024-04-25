b713a15df1a5d89bc4cbb0afce81ec7a095583ef
Lepha <lepharamchiary@gmail.com>
2024-04-25 21:56:19 +0530

    add two page trip and footer

--- src/routes/Service.js
+++ src/routes/Service.js
@@ -1,6 +1,8 @@
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import AboutImage from "../assets/service.avif";
+import Footer from "../components/Footer";
+import Trip from "../components/Trip";
 
 function Service (){
     return (
@@ -12,6 +14,8 @@ function Service (){
                 title="Service"
                 btnClass="hide"
             />
+            <Trip />
+            <Footer />
         </>
     )
 }
