ae711f860faac3828bc555281396780a6f979be5
Lepha <lepharamchiary@gmail.com>
2024-04-25 21:49:16 +0530

    new page for about section

---
+++ src/components/AboutUs.js
@@ -0,0 +1,20 @@
+import './AboutUsStyle.css';
+
+import React from 'react'
+
+function AboutUs() {
+  return (
+    <div className='about-container'>
+        <h1>Our History</h1>
+        <p>Simple Sentences Examples, 40 Example of Simple Sentence A simple sentence is a sentence that consists of just one independent clause. Here are 40 Simple Sentences Examples; 1. The course starts next Sunday. 2. I will shop at the store this weekend. buy tadasiva online buy tadasiva over the counter generic 3. She swims every morning. 4. Julie talks very fast. 5. </p>
+
+        <h1>Our Mission</h1>
+        <p>Simple Sentences Examples, 40 Example of Simple Sentence A simple sentence is a sentence that consists of just one independent clause. Here are 40 Simple Sentences Examples; 1. The course starts next Sunday. 2. I will shop at the store this weekend. buy tadasiva online buy tadasiva over the counter generic 3. She swims every morning. 4. Julie talks very fast. 5. </p>
+
+        <h1>Our Vision</h1>
+        <p>Simple Sentences Examples, 40 Example of Simple Sentence A simple sentence is a sentence that consists of just one independent clause. Here are 40 Simple Sentences Examples; 1. The course starts next Sunday. 2. I will shop at the store this weekend. buy tadasiva online buy tadasiva over the counter generic 3. She swims every morning. 4. Julie talks very fast. 5. </p>
+    </div>
+  )
+}
+
+export default AboutUs
