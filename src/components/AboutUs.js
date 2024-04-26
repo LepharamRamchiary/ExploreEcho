ebc1eb01e7bdc6d6155c640c68bd3e41f052ed20
Lepha <lepharamchiary@gmail.com>
2024-04-26 13:01:28 +0530

    change the text

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -6,13 +6,13 @@ function AboutUs() {
   return (
     <div className='about-container'>
         <h1>Our History</h1>
-        <p>Simple Sentences Examples, 40 Example of Simple Sentence A simple sentence is a sentence that consists of just one independent clause. Here are 40 Simple Sentences Examples; 1. The course starts next Sunday. 2. I will shop at the store this weekend. buy tadasiva online buy tadasiva over the counter generic 3. She swims every morning. 4. Julie talks very fast. 5. </p>
+        <p>This website is built with ReactJS, a popular JavaScript library for building user interfaces. ReactJS allows for efficient, dynamic, and interactive web development. By leveraging React's components and virtual DOM, we ensure fast rendering and seamless user experiences. This choice reflects our commitment to modern, cutting-edge technologies for creating engaging and responsive websites.</p>
 
         <h1>Our Mission</h1>
-        <p>Simple Sentences Examples, 40 Example of Simple Sentence A simple sentence is a sentence that consists of just one independent clause. Here are 40 Simple Sentences Examples; 1. The course starts next Sunday. 2. I will shop at the store this weekend. buy tadasiva online buy tadasiva over the counter generic 3. She swims every morning. 4. Julie talks very fast. 5. </p>
+        <p>Our mission is to develop products with responsive design and user-friendly interfaces. We're dedicated to crafting solutions that adapt seamlessly to various devices and screen sizes, ensuring a consistent and enjoyable experience for all users. By prioritizing accessibility and ease of use, we aim to make technology more inclusive and approachable for everyone.</p>
 
         <h1>Our Vision</h1>
-        <p>Simple Sentences Examples, 40 Example of Simple Sentence A simple sentence is a sentence that consists of just one independent clause. Here are 40 Simple Sentences Examples; 1. The course starts next Sunday. 2. I will shop at the store this weekend. buy tadasiva online buy tadasiva over the counter generic 3. She swims every morning. 4. Julie talks very fast. 5. </p>
+        <p>My vision is to become software engineer.</p>
     </div>
   )
 }
