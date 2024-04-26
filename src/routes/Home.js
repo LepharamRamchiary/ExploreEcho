dc9f2f90ca376d50252ca2bdaf001ddc8eb56d53
Lepha <lepharamchiary@gmail.com>
2024-04-26 13:03:46 +0530

    simple chnagess

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,7 @@
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import Destination from "../components/Destination";
-import Trip from "../components/Trip";
+// import Trip from "../components/Trip";
 import Footer from "../components/Footer";
 
 function Home() {
@@ -19,7 +19,7 @@ function Home() {
             />
             <Destination />
 
-            <Trip />
+            {/* <Trip /> */}
 
             <Footer />
         </>
