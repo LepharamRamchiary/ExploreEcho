ed1ee2ec93acf9f255ded5f7431b91e39e5607c6
Lepha <lepharamchiary@gmail.com>
2024-04-26 13:05:41 +0530

    simple changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,7 +13,7 @@ class Navbar extends Component {
     render() {
         return (
             <nav className="navbar-items">
-                <h1 className="navbar-logo">ExploreEcho</h1>
+                <a className="navbar-logo" href="/">ExploreEcho</a>
                 <div className="menu-icon" onClick={this.handleClick}>
                     <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
                 </div>
